d483d02bb6533d3aac31060a7b0506317bf8c5b5
Thomas Müller <mimmi20@live.de>
2015-08-25 22:49:33 +0200

    take partials from original version

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Detector [BETA] - combined browser- & feature-detection for your app' });
+  var modernizr = require('modernizr');
+
+  res.render(
+    'index',
+    {
+      title: 'Detector [BETA] - combined browser- & feature-detection for your app',
+      modernizr: modernizr
+    }
+  );
 });
 
 module.exports = router;
