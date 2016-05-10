e4311c889fb521579b0f66c2692ac35276ce7702
Thomas Müller <mimmi20@live.de>
2016-05-10 21:04:09 +0200

    add browscap-js

---
+++ public/javascripts/capabilities.js
@@ -0,0 +1,632 @@
+module.exports = {
+  "sections": [
+    {
+      "name": "css3",
+      "title": "CSS3",
+      "features": [
+        {
+          "name": "fontface",
+          "title": "@font-face",
+          "specurl": "http://dev.w3.org/csswg/css3-fonts/"
+        },
+        {
+          "name": "bgrepeatround",
+          "title": "background-repeat: round",
+          "specurl": ""
+        },
+        {
+          "name": "bgrepeatspace",
+          "title": "background-repeat: space",
+          "specurl": ""
+        },
+        {
+          "name": "backgroundsize",
+          "title": "Background Size",
+          "specurl": "http://dev.w3.org/csswg/css3-background/#the-background-size"
+        },
+        {
+          "name": "bgsizecover",
+          "title": "background-size: cover",
+          "specurl": "http://dev.w3.org/csswg/css3-background/#the-background-size"
+        },
+        {
+          "name": "borderimage",
+          "title": "Border Image",
+          "specurl": "http://dev.w3.org/csswg/css3-background/#border-images"
+        },
+        {
+          "name": "borderradius",
+          "title": "Border Radius",
+          "specurl": "http://dev.w3.org/csswg/css3-background/#corners"
+        },
+        {
+          "name": "boxshadow",
+          "title": "box-shadow",
+          "specurl": "http://dev.w3.org/csswg/css3-background/#the-box-shadow"
+        },
+        {
+          "name": "boxsizing",
+          "title": "box-sizing",
+          "specurl": ""
+        },
+        {
+          "name": "cubicbezierrange",
+          "title": "cubic-bezier values > 1",
+          "specurl": "https://bugs.webkit.org/show_bug.cgi?id=45761"
+        },
+        {
+          "name": "display-table",
+          "title": "display: table",
+          "specurl": ""
+        },
+        {
+          "name": "flexbox",
+          "title": "Flexible Box Model",
+          "specurl": "http://dev.w3.org/csswg/css3-flexbox/"
+        },
+        {
+          "name": "generatedcontent",
+          "title": "Generated Content",
+          "specurl": "http://dev.w3.org/csswg/css3-content/"
+        },
+        {
+          "name": "hsla",
+          "title": "hsla()",
+          "specurl": "http://dev.w3.org/csswg/css3-color/#hsla-color"
+        },
+        {
+          "name": "csshyphens",
+          "title": "Hyphenation",
+          "specurl": ""
+        },
+        {
+          "name": "multiplebgs",
+          "title": "Multiple backgrounds",
+          "specurl": "http://dev.w3.org/csswg/css3-background/#layering"
+        },
+        {
+          "name": "opacity",
+          "title": "opacity",
+          "specurl": "http://dev.w3.org/csswg/css3-color/#transparency"
+        },
+        {
+          "name": "overflowscrolling",
+          "title": "overflow-scrolling",
+          "specurl": ""
+        },
+        {
+          "name": "pointerevents",
+          "title": "pointer-events",
+          "specurl": "developer.mozilla.org/en/CSS/pointer-events"
+        },
+        {
+          "name": "rgba",
+          "title": "rgba()",
+          "specurl": "http://dev.w3.org/csswg/css3-color/#rgba-color"
+        },
+        {
+          "name": "textshadow",
+          "title": "text-shadow",
+          "specurl": "http://dev.w3.org/csswg/css3-text/#text-shadow"
+        },
+        {
+          "name": "userselect",
+          "title": "user-select",
+          "specurl": ""
+        },
+        {
+          "name": "cssanimations",
+          "title": "CSS Animations",
+          "specurl": "http://dev.w3.org/csswg/css3-animations/"
+        },
+        {
+          "name": "csscolumns",
+          "title": "CSS Columns",
+          "specurl": "http://dev.w3.org/csswg/css3-multicol/"
+        },
+        {
+          "name": "cssgradients",
+          "title": "CSS Gradients",
+          "specurl": "http://dev.w3.org/csswg/css3-images/#gradients"
+        },
+        {
+          "name": "cssreflections",
+          "title": "CSS Reflections"
+        },
+        {
+          "name": "csstransforms",
+          "title": "CSS 2D Transforms",
+          "specurl": "http://dev.w3.org/csswg/css3-2d-transforms/"
+        },
+        {
+          "name": "csstransforms3d",
+          "title": "CSS 3D Transforms",
+          "specurl": "http://dev.w3.org/csswg/css3-3d-transforms/"
+        },
+        {
+          "name": "csstransitions",
+          "title": "CSS Transitions",
+          "specurl": "http://dev.w3.org/csswg/css3-transitions/"
+        },
+        {
+          "name": "appearance",
+          "title": "Appearance",
+          "specurl": "https://developer.mozilla.org/en-US/docs/Web/CSS/-moz-appearance"
+        },
+        {
+          "name": "backdropfilter",
+          "title": "CSS Backdrop Filter",
+          "specurl": "https://drafts.fxtf.org/filters-2/#BackdropFilterProperty"
+        },
+        {
+          "name": "backgroundblendmode",
+          "title": "CSS Background Blend Mode"
+        },
+        {
+          "name": "backgroundcliptext",
+          "title": "CSS Background Clip Text",
+          "specurl": "https://developer.mozilla.org/en-US/docs/Web/CSS/background-clip"
+        },
+        {
+          "name": "checked",
+          "title": "CSS :checked pseudo-selector",
+          "specurl": ""
+        }
+      ]
+    },
+    {
+      "name": "html5",
+      "title": "HTML5",
+      "features": [
+        {
+          "name": "applicationcache",
+          "title": "Application Cache",
+          "specurl": "http://www.whatwg.org/specs/web-apps/current-work/multipage/offline.html"
+        },
+        {
+          "name": "canvas",
+          "title": "Canvas",
+          "specurl": "http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html"
+        },
+        {
+          "name": "canvastext",
+          "title": "Canvas Text",
+          "specurl": "http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#text-0"
+        },
+        {
+          "name": "canvasblending",
+          "title": "canvas blending support",
+          "specurl": "https://dvcs.w3.org/hg/FXTF/rawfile/tip/compositing/index.html#blending"
+        },
+        {
+          "name": "canvaswinding",
+          "title": "canvas winding support",
+          "specurl": ""
+        },
+        {
+          "name": "details",
+          "title": "<details>",
+          "specurl": ""
+        },
+        {
+          "name": "devicemotion",
+          "title": "Device Motion Event",
+          "specurl": "http://dev.w3.org/geo/api/spec-source-orientation.html"
+        },
+        {
+          "name": "deviceorientation",
+          "title": "Device Orientation Event",
+          "specurl": "http://dev.w3.org/geo/api/spec-source-orientation.html"
+        },
+        {
+          "name": "draganddrop",
+          "title": "Drag and Drop",
+          "specurl": "http://www.whatwg.org/specs/web-apps/current-work/multipage/dnd.html"
+        },
+        {
+          "name": "file",
+          "title": "File API",
+          "specurl": "http://dev.w3.org/2006/webapi/FileAPI/"
+        },
+        {
+          "name": "hashchange",
+          "title": "hashchange Event",
+          "specurl": "http://www.whatwg.org/specs/web-apps/current-work/multipage/webappapis.html#handler-window-onhashchange"
+        },
+        {
+          "name": "history",
+          "title": "History Management",
+          "specurl": "http://www.whatwg.org/specs/web-apps/current-work/multipage/history.html#the-history-interface"
+        },
+        {
+          "name": "audio",
+          "title": "HTML5 Audio",
+          "specurl": "http://www.whatwg.org/specs/web-apps/current-work/multipage/the-video-element.html#the-audio-element",
+          "subfeatures": [
+            {
+              "name": "audio.mp3",
+              "title": "MP3"
+            },
+            {
+              "name": "audio.wav",
+              "title": "WAV"
+            },
+            {
+              "name": "audio.m4a",
+              "title": "M4A"
+            },
+            {
+              "name": "audio.ogg",
+              "title": "Ogg"
+            },
+            {
+              "name": "audio.opus",
+              "title": "Opus"
+            }
+          ]
+        },
+        {
+          "name": "audioloop",
+          "title": "Audio Loop Attribute"
+        },
+        {
+          "name": "video",
+          "title": "HTML5 Video",
+          "specurl": "http://www.whatwg.org/specs/web-apps/current-work/multipage/the-video-element.html#the-video-element",
+          "subfeatures": [
+            {
+              "name": "video-h264",
+              "title": "H.264"
+            },
+            {
+              "name": "video-ogg",
+              "title": "Ogg"
+            },
+            {
+              "name": "video-webm",
+              "title": "WebM"
+            }
+          ]
+        },
+        {
+          "name": "indexeddb",
+          "title": "IndexedDB",
+          "specurl": "http://www.w3.org/TR/IndexedDB/"
+        },
+        {
+          "name": "localstorage",
+          "title": "localStorage",
+          "specurl": "http://dev.w3.org/html5/webstorage/#the-localstorage-attribute"
+        },
+        {
+          "name": "meter",
+          "title": "<meter>",
+          "specurl": ""
+        },
+        {
+          "name": "postmessage",
+          "title": "Cross-window Messaging",
+          "specurl": "http://www.whatwg.org/specs/web-apps/current-work/multipage/comms.html#web-messaging"
+        },
+        {
+          "name": "progressbar",
+          "title": "<progress>",
+          "specurl": ""
+        },
+        {
+          "name": "sessionstorage",
+          "title": "sessionStorage",
+          "specurl": "http://dev.w3.org/html5/webstorage/#the-sessionstorage-attribute"
+        },
+        {
+          "name": "sharedworkers",
+          "title": "Shared Workers",
+          "specurl": "http://dev.w3.org/html5/workers/#shared-workers-introduction"
+        },
+        {
+          "name": "websockets",
+          "title": "Web Sockets",
+          "specurl": "http://dev.w3.org/html5/websockets/"
+        },
+        {
+          "name": "websqldatabase",
+          "title": "Web SQL Database",
+          "specurl": "http://www.w3.org/TR/webdatabase/"
+        },
+        {
+          "name": "webworkers",
+          "title": "Web Workers",
+          "specurl": "http://www.whatwg.org/specs/web-apps/current-work/complete/workers.html"
+        },
+        {
+          "name": "adownload",
+          "title": "a[download] Attribute",
+          "specurl": "https://developers.whatwg.org/links.html#downloading-resources"
+        },
+        {
+          "name": "bdi",
+          "title": "<bdi> Element",
+          "specurl": "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/bdi"
+        },
+        {
+          "name": "bgpositionshorthand",
+          "title": "Background Position Shorthand",
+          "specurl": "https://www.w3.org/TR/css3-background/#background-position"
+        },
+        {
+          "name": "bgpositionxy",
+          "title": "Background Position XY",
+          "specurl": "https://www.w3.org/TR/css3-background/#background-position"
+        }
+      ]
+    },
+    {
+      "name": "inputtypes",
+      "title": "Input Types",
+      "features": [
+        {
+          "name": "search",
+          "title": "search",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#text-state-and-search-state"
+        },
+        {
+          "name": "tel",
+          "title": "tel",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#telephone-state"
+        },
+        {
+          "name": "url",
+          "title": "url",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#url-state"
+        },
+        {
+          "name": "email",
+          "title": "email",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#e-mail-state"
+        },
+        {
+          "name": "datetime",
+          "title": "datetime",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#date-and-time-state"
+        },
+        {
+          "name": "date",
+          "title": "date",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#date-state"
+        },
+        {
+          "name": "month",
+          "title": "month",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#month-state"
+        },
+        {
+          "name": "week",
+          "title": "week",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#week-state"
+        },
+        {
+          "name": "time",
+          "title": "time",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#time-state"
+        },
+        {
+          "name": "datetime-local",
+          "title": "datetime-local",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#local-date-and-time-state"
+        },
+        {
+          "name": "number",
+          "title": "number",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#number-state"
+        },
+        {
+          "name": "range",
+          "title": "range",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#range-state"
+        },
+        {
+          "name": "color",
+          "title": "color",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#color-state"
+        }
+      ]
+    },
+    {
+      "name": "input",
+      "title": "Input Attributes",
+      "features": [
+        {
+          "name": "autocomplete",
+          "title": "autocomplete",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#the-autocomplete-attribute"
+        },
+        {
+          "name": "autofocus",
+          "title": "autofocus",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#attr-fe-autofocus"
+        },
+        {
+          "name": "list",
+          "title": "list",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#attr-input-list"
+        },
+        {
+          "name": "placeholder",
+          "title": "placeholder",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#the-placeholder-attribute"
+        },
+        {
+          "name": "max",
+          "title": "max",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#the-min-and-max-attributes"
+        },
+        {
+          "name": "min",
+          "title": "min",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#the-min-and-max-attributes"
+        },
+        {
+          "name": "multiple",
+          "title": "multiple",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#the-multiple-attribute"
+        },
+        {
+          "name": "pattern",
+          "title": "pattern",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#the-pattern-attribute"
+        },
+        {
+          "name": "required",
+          "title": "required",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#the-required-attribute"
+        },
+        {
+          "name": "step",
+          "title": "step",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#the-step-attribute"
+        },
+        {
+          "name": "capture",
+          "title": "input[capture] Attribute",
+          "specurl": "https://www.w3.org/TR/html-media-capture/"
+        }
+      ]
+    },
+    {
+      "name": "misc",
+      "title": "Miscellaneous",
+      "features": [
+        {
+          "name": "cookies",
+          "title": "Cookies",
+          "specurl": ""
+        },
+        {
+          "name": "customprotocolhandler",
+          "title": "Custom Protocol Handlers",
+          "specurl": ""
+        },
+        {
+          "name": "datauri",
+          "title": "data URIs",
+          "specurl": ""
+        },
+        {
+          "name": "emoji",
+          "title": "Emoji",
+          "specurl": "http://en.wikipedia.org/wiki/Emoji"
+        },
+        {
+          "name": "fullscreen",
+          "title": "Fullscreen API",
+          "specurl": "https://developer.mozilla.org/en/API/Fullscreen"
+        },
+        {
+          "name": "geolocation",
+          "title": "Geolocation API",
+          "specurl": "http://dev.w3.org/geo/api/spec-source.html"
+        },
+        {
+          "name": "inlinesvg",
+          "title": "Inline SVG",
+          "specurl": "http://dev.w3.org/html5/spec/Overview.html#svg-0"
+        },
+        {
+          "name": "smil",
+          "title": "SMIL",
+          "specurl": "http://www.w3.org/TR/REC-smil/"
+        },
+        {
+          "name": "svg",
+          "title": "SVG",
+          "specurl": "http://www.w3.org/TR/SVG/"
+        },
+        {
+          "name": "svgclippaths",
+          "title": "SVG Clip paths",
+          "specurl": "http://www.w3.org/TR/SVG/masking.html#ClipPathProperty"
+        },
+        {
+          "name": "touch",
+          "title": "Touch Events",
+          "specurl": "http://www.w3.org/TR/2011/WD-touch-events-20110505/"
+        },
+        {
+          "name": "webgl",
+          "title": "WebGL",
+          "specurl": "http://www.khronos.org/registry/webgl/specs/latest/"
+        },
+        {
+          "name": "webp",
+          "title": "WebP",
+          "specurl": "http://code.google.com/speed/webp/"
+        },
+        {
+          "name": "ambientlight",
+          "title": "Ambient Light Events",
+          "specurl": "https://www.w3.org/TR/ambient-light/"
+        },
+        {
+          "name": "apng",
+          "title": "Animated PNG",
+          "specurl": "https://en.wikipedia.org/wiki/APNG"
+        },
+        {
+          "name": "atobbtoa",
+          "title": "Base 64 encoding/decoding",
+          "specurl": "https://www.w3.org/TR/html5/webappapis.html#windowbase64"
+        },
+        {
+          "name": "batteryapi",
+          "title": "Battery API",
+          "specurl": "https://developer.mozilla.org/en/DOM/window.navigator.mozBattery"
+        },
+        {
+          "name": "beacon",
+          "title": "Beacon API",
+          "specurl": "https://dvcs.w3.org/hg/webperf/raw-file/tip/specs/Beacon/Overview.html"
+        },
+        {
+          "name": "fileapi",
+          "title": "File API",
+          "specurl": "https://www.w3.org/TR/FileAPI/#creating-revoking",
+          "subfeatures": [
+            {
+              "name": "blobconstructor",
+              "title": "Blob constructor"
+            },
+            {
+              "name": "bloburls",
+              "title": "Blob URLs"
+            },
+            {
+              "name": "blobworkers",
+              "title": "Workers from Blob URIs"
+            }
+          ]
+        },
+        {
+          "name": "classlist",
+          "title": "classlist",
+          "specurl": "https://developer.mozilla.org/en/DOM/element.classList"
+        },
+        {
+          "name": "contenteditable",
+          "title": "Content Editable",
+          "specurl": "https://html.spec.whatwg.org/multipage/interaction.html#contenteditable"
+        }
+      ]
+    },
+    {
+      "name": "es6",
+      "title": "ECMAScript 6",
+      "features": [
+        {
+          "name": "contains",
+          "title": "ES5 String.prototype.contains",
+          "specurl": ""
+        }
+      ]
+    }
+  ]
+};
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  var Browscap = require('browscap-js'),
+    browscap = new Browscap(),
+    ua = req.headers['user-agent'],
+    browser;
+
+  console.log('User-Agent: ' + ua);
+
+  browser = browscap.getBrowser(ua);
+  console.log(JSON.stringify(browser));
+
   res.render(
     'index',
     {
-      title: 'Detector [BETA] - combined browser- & feature-detection for your app'
+      title: 'Detector [BETA] - combined browser- & feature-detection for your app',
+      ua: ua,
+      browsername: browser['Browser'],
+      devicename: browser['Device_Name'],
+      mobile: browser['isMobileDevice'],
+      tablet: browser['isTablet'],
+      bot: browser['Crawler']
     }
   );
 });
