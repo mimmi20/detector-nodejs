66e2998a07fe5269a6f736bdff7637cecbcc075a
Thomas Müller <mimmi20@live.de>
2016-06-06 20:15:15 +0200

    reformat output and layout

--- public/javascripts/show-capabilities-haz.js
+++ public/javascripts/show-capabilities-haz.js
@@ -4,51 +4,51 @@ jQuery('span[data-capability]').each(function() {
     t = typeof result[data];
 
   if (!result.hasOwnProperty(data) || t === 'undefined') {
-    jQuery(this).addClass('label label-info support').html('undefined');
+    jQuery(this).addClass('label label-pill label-info support').html('undefined');
 
     return;
   }
 
   if (t === 'boolean') {
     if (result[data]) {
-      jQuery(this).addClass('label label-success support').html('true');
+      jQuery(this).addClass('label label-pill label-success support').html('true');
     } else {
-      jQuery(this).addClass('label label-danger support').html('false');
+      jQuery(this).addClass('label label-pill label-danger support').html('false');
     }
 
     return;
   }
 
   if (result[data] === 't' || result[data] === true) {
-    jQuery(this).addClass('label label-success support').html('true');
+    jQuery(this).addClass('label label-pill label-success support').html('true');
 
     return;
   } else if (result[data] === 'f' || result[data] === false) {
-    jQuery(this).addClass('label label-danger support').html('false');
+    jQuery(this).addClass('label label-pill label-danger support').html('false');
 
     return;
   } else if (result[data] === 'n' || result[data] === null) {
-    jQuery(this).addClass('label label-warning support').html('null');
+    jQuery(this).addClass('label label-pill label-warning support').html('null');
 
     return;
   } else if (result[data] === 'e' || result[data] === '') {
-    jQuery(this).addClass('label label-danger support').html('false');
+    jQuery(this).addClass('label label-pill label-danger support').html('false');
 
     return;
   } else if (result[data] === 'p' || result[data] === 'probably') {
-    jQuery(this).addClass('label label-warning support').html('probably');
+    jQuery(this).addClass('label label-pill label-warning support').html('probably');
 
     return;
   } else if (result[data] === 'm' || result[data] === 'maybe') {
-    jQuery(this).addClass('label label-warning support').html('maybe');
+    jQuery(this).addClass('label label-pill label-warning support').html('maybe');
 
     return;
   } else if (result[data] === 'u') {
-    jQuery(this).addClass('label label-danger support').html('n/a');
+    jQuery(this).addClass('label label-pill label-danger support').html('n/a');
 
     return;
   } else {
-    jQuery(this).addClass('label label-info support').html(result[data]);
+    jQuery(this).addClass('label label-pill label-info support').html(result[data]);
 
     return;
   }
@@ -56,22 +56,22 @@ jQuery('span[data-capability]').each(function() {
   if (typeof result[data] !== 'undefined') {
     switch (result[data]) {
       case 't':
-        jQuery(this).addClass('label label-success support').html('true');
+        jQuery(this).addClass('label label-pill label-success support').html('true');
         break;
       case 'f':
-        jQuery(this).addClass('label label-danger support').html('false');
+        jQuery(this).addClass('label label-pill label-danger support').html('false');
         break;
       case 'p':
-        jQuery(this).addClass('label label-warning support').html('probably');
+        jQuery(this).addClass('label label-pill label-warning support').html('probably');
         break;
       case 'm':
-        jQuery(this).addClass('label label-warning support').html('maybe');
+        jQuery(this).addClass('label label-pill label-warning support').html('maybe');
         break;
       case 'u':
-        jQuery(this).addClass('label label-info support').html('undefined');
+        jQuery(this).addClass('label label-pill label-info support').html('undefined');
         break;
       default:
-        jQuery(this).addClass('label label-info support').html(result[data]);
+        jQuery(this).addClass('label label-pill label-info support').html(result[data]);
     }
   }
 });
